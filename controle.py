ea92c135c73479c61b64b174d3ac339a42c9b0b7
Daniel Fonseca Alves <dalraf@gmail.com>
2021-04-28 14:09:30 -0300

    Adicionado mensagem explicativa

--- controle.py
+++ controle.py
@@ -6,8 +6,12 @@ pygame.font.init()
 class Controle():
     def __init__(self):
         self.font = pygame.font.SysFont('Sans', 35)
-        self.controle = self.font.render('Pressione ENTER para continuar', False, (255, 255, 255))
-        self.rect = self.controle.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        self.controle1 = self.font.render('Pressione ENTER para continuar', False, (255, 255, 255))
+        self.controle2 = self.font.render('Setas Movimentam, Espaço Atira', False, (255, 255, 255))
+        self.rect1 = self.controle1.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        self.rect2 = self.controle2.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2 + 50))
 
     def draw(self, screen):
-        screen.blit(self.controle,self.rect)
+        screen.blit(self.controle1,self.rect1)
+        screen.blit(self.controle2,self.rect2)
+        

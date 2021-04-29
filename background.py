4e7b49ee9e45955afcfcba1c052fc1ecdd0bfb20
Daniel Fonseca Alves <dalraf@gmail.com>
2021-04-29 14:19:23 -0300

    Alterado Forma de background

--- background.py
+++ background.py
@@ -5,8 +5,7 @@ class Background():
 
     def __init__(self):
         self.backgroundwidth = SCREEN_WIDTH * 3
-        self.spritesheet = Spritesheet(resource_path("images/bg_reference.png"))
-        self.background = self.spritesheet.image_at((15,15,773,190))
+        self.background = pygame.image.load(resource_path("images/bg.png"))
         self.background = pygame.transform.scale(self.background, (self.backgroundwidth, SCREEN_HEIGHT))
         self.rect = self.background.get_rect()
         self.step = 0

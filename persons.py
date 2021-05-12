577a17b65849822e4d08b1a9103f842d84b20417
Daniel Fonseca Alves <dalraf@gmail.com>
2021-05-12 18:20:58 -0300

    Melhoria de código

--- persons.py
+++ persons.py
@@ -60,9 +60,7 @@ class SpritePerson(pygame.sprite.Sprite):
 
         if self.images_list == images_list:
             self.counter = (self.counter + 1) % (len(images_list) * self.sprint)
-            self.image_raw = load(
-                self.images_list[int(self.counter / self.sprint)]
-            )
+            self.image_raw = load(self.images_list[int(self.counter / self.sprint)])
             self.image = pygame.transform.scale(
                 self.image_raw, ((int(80 * RESIZE_FACTOR), int(80 * RESIZE_FACTOR)))
             )
